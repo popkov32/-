fd7b605380bcb0fe759ed66c6b5de692737a36cf
popkov32 <popkov32@yandex.ru>
2024-07-18 11:41:21 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+qtty = (12)
+hours = (1.5)
+name_of_course = ('Python')
+time_for_each = (hours / qtty)
+print('Курс:',name_of_course,',всего задач:',qtty,
+      ',затрачено часов:',hours,
+      ',среднее время выполнения',time_for_each,'часа.')
