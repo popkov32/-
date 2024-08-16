eab8dae523f711a08728ed06f9df356b7e02cf68
popkov32 <popkov32@yandex.ru>
2024-08-16 11:28:22 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,14 @@
+
+
+from true_math import divide as td
+from fake_math import divide as fd
+
+result1 = fd(69, 3)
+result2 = fd(3, 0)
+result3 = td(49, 7)
+result4 = td(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
